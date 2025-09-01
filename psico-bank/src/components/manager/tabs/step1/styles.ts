659abb0d3eaa3c53fdc86208e522b87d9d42cf8e
String <novadeveloper11@gmail.com>
2025-09-01 17:03:13 -0300

    chore: add responsive to inputs from step 1

--- psico-bank/src/components/manager/tabs/step1/styles.ts
+++ psico-bank/src/components/manager/tabs/step1/styles.ts
@@ -11,4 +11,12 @@ export const FormItem = styled.div`
 	display: flex;
 	width: 100%;
 	gap: 1.25rem;
+
+	@media (max-width: 768px) {
+		flex-direction: column;
+
+		> * {
+			width: 100% !important;
+		}
+	}
 `;
